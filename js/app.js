51400e75a338ce8f87c8d0ee90fed6b1e9306467
Ryan DeSha <rdesha0218@yahoo.com>
2015-09-28 19:30:28 -0700

    changes 1

--- js/app.js
+++ js/app.js
@@ -1,90 +1,84 @@
-//Use original html and css
-//When page loads new game should start
-//When new game starts, secret 1 - 100 num should be generated
-//User should get feedback about each guess
-//Hotter colder 50 > away ice cold, between 30 to 50 cold, 
-//	between 20 and 30 warm between 10 and 20 hot. 1 - 10 very hot
-//Feedback should appear in div#feedback, page loads sets this to Make Your Guess
-//SHould track how many guesses the user had made, appear in span#count,
-//	defaults to 0 when page loads
-//SHould supply users with numbers they've guessed so far
-//	added as an <li> to ul#guessList
-//New game button should trigger new game function
-
-var randomnumber, randNum, hotnum, guess = '';
 
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
+    $('button').click(function() {
+        player(); 
+        return false;
+    });
+
+    $(function() {
+        $("form").submit(function() {
+           player();
+            return false; 
+        });
+    });
+
+	/// Display information modal box 
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
 
-  	/*--- Hide information modal box ---*/
+  	//--- Hide information modal box
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+});	
 
-});
-/*
-function game(guess) {
-		
+function play() {
+//Logic function that determines hot and cold
+function difference() {
+	var hotnum = Math.abs(guess - randomnumber);
+	return hotnum;
 }
-*/
-	//Logic function that determines hot and cold
+function hotter() {
 
-
-	function hotter(choice) {
-		var guess = choice;
-		return guess;
-
-		hotnum = Math.abs(guess - randomnumber);
-
-			if (randomnumber== guess) {
-				return "Your guess was right!";
-				level_four_diagnostic();
-			}
-			else if	(hotnum > 30 && hotnum < 50) {
-				return "Guess is Ice Cold!";
-				level_four_diagnostic();
-			}
-			else if (hotnum  > 20 && hotnum < 30) {
-				return "Warm";
-				level_four_diagnostic();
-			}
-			else if (hotnum > 10 && hotnum < 20) {
-				level_four_diagnostic();
-				return "Hot";
-			}
-			else if (hotnum > 1 && hotnum < 10) {
-				level_four_diagnostic();
-				return "Very Hot";
-			}
-
-			else {
-				return "Not even close"
-				level_four_diagnostic();
-			}
+	if (randomnumber == guess) {
+		return "Your guess was right!";
+	}
+	else if	(hotnum > 30 && choice < 50) {
+		return "Guess is Ice Cold!";
+	}
+	else if (hotnum  > 20 && hotnum < 30) {
+		return "Warm";
+	}
+	else if (hotnum > 10 && hotnum < 20) {
+		return "Hot";
+	}
+	else if (hotnum > 1 && hotnum < 10) {
+		return "Very Hot";
 	}
 
-//Resets game data and starts new game
-function newGame() {
-	randNum();
-	guess = '';
-	hotnum = '';
+	else {
+		return "Not even close"
+	}
 }
 
+
+function player() {
+	var guess = $('.text').val();
+    $('ul#guessList').append('<li>' + guess + '</li>');
+    return guess;
+}
 //Random number generator betweeen 1 and 100
 function randNum() {
-	randomnumber = Math.floor(Math.random()*101);
+	var randomnumber = Math.floor(Math.random()*101);
 	//var randomnumber = Math.floor(Math.random()*101);
 	return randomnumber;
 	console.log(randomnumber);
 }
 
-function level_four_diagnostic() {
-	console.log("Guess is" + guess);
+
+ function newGame() {
+ 	randNum();
+ 	var guess = 0;
+ 	var hotnum = 0;
+ 	level_four_diagnostic();
+ }
+
+ function level_four_diagnostic() {
+	console.log("Guess is " + guess);
 	console.log("Random is " + randomnumber);
 	console.log("Hotnum is " + hotnum);
+}
+
 }
