f8718c04d6233de108401f3e309f91e1da30c8cf
Ryan DeSha <rdesha0218@yahoo.com>
2015-09-29 19:03:04 -0700

    changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,82 @@
+newGame();
+  
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+$(document).ready(function() {
+    $('button').click(function() {
+        player();
+        return false;
+    });
 
-  	});
+    $(function() {
+        $("form").submit(function() {
+            player();
+            return false;
+        });
+    });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+    /// Display information modal box 
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
 
+    });
+
+    //--- Hide information modal box
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
 });
 
+    //Logic function that determines hot and cold
+    function difference() {
+        var hotnum = Math.abs(guess - randomnumber);
+        return hotnum;
+    }
 
+    function hotter() {
+
+        if (randomnumber == guess) {
+            return "Your guess was right!";
+        } else if (hotnum > 30 && choice < 50) {
+            return "Guess is Ice Cold!";
+        } else if (hotnum > 20 && hotnum < 30) {
+            return "Warm";
+        } else if (hotnum > 10 && hotnum < 20) {
+            return "Hot";
+        } else if (hotnum > 1 && hotnum < 10) {
+            return "Very Hot";
+        } else {
+            return "Not even close"
+        }
+    }
+
+    function player() {
+            var guess = $('.text').val();
+            $('ul#guessList').append('<li>' + guess + '</li>');
+            return guess;
+        }
+        //Random number generator betweeen 1 and 100
+    function randNum() {
+        var randomnumber = Math.floor(Math.random() * 101);
+        //var randomnumber = Math.floor(Math.random()*101);
+        return randomnumber;
+        console.log(randomnumber);
+    }
+
+    function newGame() {
+    	/*
+        var guess = 0;
+        var hotnum = 0;
+        var randomnumber = 0;
+        */
+        randNum();
+    }
+
+    function level_four_diagnostic() {
+    	var guess;
+  var hotnum;
+  var randomnumber;
+    	player();
+        console.log("Guess is " + guess);
+        console.log("Random is " + randomnumber);
+        console.log("Hotnum is " + hotnum);
+    }
